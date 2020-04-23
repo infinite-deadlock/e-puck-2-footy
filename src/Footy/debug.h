27ba3c772bd967810131172345462b0b81ca7599
Pierre <pierre.oppliger@epfl.ch>
2020-04-23 16:21:28 +0200

    Correction d'un commentaire

--- src/Footy/debug.h
+++ src/Footy/debug.h
@@ -27,7 +27,7 @@ void debug_am_i_responding(void);
 */
 void debug_send_uint32_to_computer(uint32_t data);
 /**
-* @brief   Sends a buffer of integer
+* @brief   Sends a buffer of integer in user-friendly style
 *
 * @param data			Buffer to send
 * @param size			Size of the buffer is nb uint8_t
